63d30b9e85ac4dda0a20439d9355b311b572383e
STARTIT-22\Student <academy@startit.lviv.ua>
2024-11-09 17:52:13 +0200

    code

---
+++ Test1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    public Transform transformCube;
+    public Transform transformSphere;
+    public float x;
+    public float y;
+    public int speed;
+
+    private void Update()
+    {
+        x += speed * Time.deltaTime;
+        y += speed * Time.deltaTime;
+        transformCube.position = new Vector3(x, y, 25f);
+    }
+}
