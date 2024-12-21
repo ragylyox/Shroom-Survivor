a6adb617da9640dc24373025047e8b95ac8ea8e9
GokuBlack-n1 <maksimstrutinskij875@gmail.com>
2024-12-21 17:56:36 +0200

    mashrooms

---
+++ Assets/Shroom.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shroom : MonoBehaviour
+{
+   
+    public GameObject Text;
+
+    public void OnTriggerStay(Collider other)
+    {
+        //Text.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            inventoryscript.shroomcounter++;
+            Debug.Log(inventoryscript.shroomcounter);
+            Destroy(gameObject);
+            
+
+        }
+    }
+
+
+    public void OnTriggerExit(Collider other)
+    {
+    }
+}
