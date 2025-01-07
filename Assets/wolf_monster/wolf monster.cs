87299bdbda2baec2b5990dd0c9525b5e2d3dffd1
STARTIT-19\Student <academy@startit.lvivlua>
2025-01-07 12:47:05 +0200

    wolf test2

--- Assets/wolf_monster/wolf monster.cs
+++ Assets/wolf_monster/wolf monster.cs
@@ -1,18 +1,38 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class wolfmonster : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public NavMeshAgent myAgent;
+    private float distance;
+    public Transform target;
+    public Animator myAnimator;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance > 15)
+        {
+            myAgent.enabled = false;
+            myAnimator.Play("Idle");
+        }
+        if (distance < 15 & distance > 1)
+        {
+            myAgent.enabled = true;
+            myAgent.SetDestination(target.position);
+            myAnimator.Play("Run");
+        }
+        if (distance <= 1)
+        {
+            myAnimator.Play("Attak");
+            myAgent.enabled = false;
+        }
     }
 }
+
+
